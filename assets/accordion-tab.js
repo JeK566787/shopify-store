5c3b5d8e5477adfce1cd2873fea9099cf63e6ced
Evgeniy Pavlichenko <92581680+JeK566787@users.noreply.github.com>
2025-05-11 19:07:52 +0300

    add changes in readme

--- assets/accordion-tab.js
+++ assets/accordion-tab.js
@@ -1,30 +1,7 @@
 class AccordionTab extends HTMLElement {
     constructor() {
         super();
-        //     this.attachShadow({ mode: 'open' });
-        //     this.shadowRoot.innerHTML = `
-        //   <style>
-        //     :host {
-        //       display: block;
-        //       border: 1px solid #ccc;
-        //       border-radius: 4px;
-        //       margin: 10px 0;
-        //     }
-        //     .header {
-        //       background-color: #f1f1f1;
-        //       padding: 10px;
-        //       cursor: pointer;
-        //     }
-        //     .content {
-        //       display: none;
-        //       padding: 10px;
-        //     }
-        //   </style>
-        //   <div class="header">Accordion Tab</div>
-        //   <div class="content">
-        //     <slot></slot>
-        //   </div>
-        // `;
+
     }
 
     connectedCallback() {
@@ -32,21 +9,16 @@ class AccordionTab extends HTMLElement {
         this.tabHeader = this.querySelector('[data-header]');
         this.tabContent = this.querySelector('[data-content]');
         this.tabHeader.addEventListener('click', this.handleToggle.bind(this));
-        // this.shadowRoot.querySelector('.header').addEventListener('click', () => {
-        //     const content = this.shadowRoot.querySelector('.content');
-        //     content.style.display = content.style.display === 'block' ? 'none' : 'block';
-        // });
     }
 
     disconnectedCallback() {
-        // this.shadowRoot.querySelector('.header').removeEventListener('click', this.toggleContent);
+
     }
 
     handleToggle() {
         console.log("Header clicked");
         this.toggleAttribute('open')
-        // const content = this.tabContent;
-        // content.style.display = content.style.display === 'block' ? 'none' : 'block';
+
     }
 }
 
